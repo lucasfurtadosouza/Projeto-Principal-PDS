c63b5138bca3917f55a9d8c72440a654ab850826
lucastozo <lucastozo7@gmail.com>
2022-08-23 14:53:12 -0400

    Cliente.cs
    adicionado

--- Projeto_PDS/Models/Cliente.cs
+++ Projeto_PDS/Models/Cliente.cs
@@ -8,6 +8,16 @@ namespace Projeto_PDS.Models
 {
     public class Cliente
     {
-        
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Cpf { get; set; }
+        public string Telefone { get; set; }
+        public string Endereco { get; set; }
+        public string Rg { get; set; }
+        public string Data { get; set; }
+        public string Sexo { get; set; }
+        public string RendaFamiliar { get; set; }
+        public string Foto { get; set; }
     }
 }
