0d6c337cdf79e498ce09ea5b6f0570d51bc74054
TaynaraPereira <thaypereira69021@gmail.com>
2022-08-23 14:53:36 -0400

    Update Despesa.cs
    Despesa

--- Projeto_PDS/Models/Despesa.cs
+++ Projeto_PDS/Models/Despesa.cs
@@ -8,5 +8,16 @@ namespace Projeto_PDS.Models
 {
     public class Despesa
     {
+        public int id { get; set; }
+
+        public double Valor {get; set;}
+
+        public string Data_Vencimento { get; set; }
+        public string Data_Pagamento { get; set; }
+        public string Forma_Pagamento { get; set; }
+        public string Descrição { get; set; }
+
+
+
     }
 }
