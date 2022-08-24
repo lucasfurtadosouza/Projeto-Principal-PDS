0282cfac55bb5c63b9a9b48993979a53274e4cb5
moria10092004 <eduarda.moria2004@gmail.com>
2022-08-24 13:15:39 -0400

    Fornecedor.cs

--- Projeto_PDS/Models/Fornecedor.cs
+++ Projeto_PDS/Models/Fornecedor.cs
@@ -8,5 +8,12 @@ namespace Projeto_PDS.Models
 {
     public class Fornecedor
     {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string  Razao { get; set; }
+        public string  Cnpj{ get; set; }
+        public string  Email { get; set; }
+        public string  Endereco { get; set; }
+        public string  Telefone{ get; set; }
     }
 }
