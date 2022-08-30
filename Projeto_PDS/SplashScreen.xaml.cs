80103ae52f86c5d9230d7d8d0c99124cbfa4889b
Lucas_Furtado_Souza <lucasfurtadosouza32@gmail.com>
2022-08-30 14:51:12 -0400

    Add Windows
    Add Windows

---
+++ Projeto_PDS/SplashScreen.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Projeto_PDS.Views;
+
+namespace Projeto_PDS
+{
+    /// <summary>
+    /// Lógica interna para SplashScreen.xaml
+    /// </summary>
+    public partial class SplashScreen : Window
+    {
+        public SplashScreen()
+        {
+            InitializeComponent();
+            var form = new WindowTermos();
+            form.Show();
+            this.Close();
+        }
+    }
+}
