61a0600952dcf72a772733e0e0f91719655ee3bb
lucastozo <lucastozo7@gmail.com>
2022-09-13 16:41:02 -0400

    PageCliente & PageProduto

--- Projeto_PDS/Views/PageCliente.xaml.cs
+++ Projeto_PDS/Views/PageCliente.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Projeto_PDS.Models;
 
 namespace Projeto_PDS.Views
 {
@@ -23,6 +24,59 @@ namespace Projeto_PDS.Views
         public PageCliente()
         {
             InitializeComponent();
+        }
+
+        private Cliente _cliente = new Cliente();
+
+        public PageCliente(Cliente cliente)
+        {
+            _cliente = cliente;
+            InitializeComponent();
+            Loaded += ClienteWindow_Loaded;
+        }
+        private void ClienteWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btSalvar_Click(object sender, RoutedEventArgs e)
+        {
+            _cliente.Nome = txtNome.Text;
+            _cliente.Email = txtEmail.Text;
+            _cliente.Cpf = txtCpf.Text;
+            _cliente.Telefone = txtTelefone.Text;
+            _cliente.Endereco = txtEndereco.Text;
+            _cliente.Rg = txtRg.Text;
+
+            if (dtDataNasc.SelectedDate != null)
+            {
+                _cliente.DataNasc = (DateTime)dtDataNasc.SelectedDate;
+            }
+
+            _cliente.Sexo = cbSexo.Text;
+            _cliente.RendaFamiliar = txtRenda.Text;
+
+            try
+            {
+                var dao = new ClienteDAO();
+                if (_cliente.Id > 0)
+                {
+                    dao.Update(_cliente);
+                    MessageBox.Show("Informações Atualizadas com Sucesso", "Cadastro Atualizado", MessageBoxButton.OK, MessageBoxImage.Information);
+                    var form = new Projeto_PDS.Views.WindowClienteList();
+                    form.Show();
+                }
+                else
+                {
+                    dao.Insert(_cliente);
+                    MessageBox.Show("Informações Salvas com Sucesso", "Cadastro Salvo", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
         }
     }
 }
