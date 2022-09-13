ef9d9653aed755d000298e40413e6f9253cdd92f
TaynaraPereira <thaypereira69021@gmail.com>
2022-09-13 16:42:58 -0400

    Update PageDespesa.xaml.cs
    Codigo da Page de Despesa

--- Projeto_PDS/Views/PageDespesa.xaml.cs
+++ Projeto_PDS/Views/PageDespesa.xaml.cs
@@ -21,9 +21,57 @@ namespace Projeto_PDS.Views
     /// </summary>
     public partial class PageDespesa : Page
     {
+        public Despesa _despesa = new Despesa();
         public PageDespesa()
         {
             InitializeComponent();
+        }
+        public PageDespesa(Despesa despesa)
+        {
+            _despesa = despesa;
+            InitializeComponent();
+            Loaded += DespesaWindow_Loaded;
+        }
+        private void DespesaWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            txtValor.Focus();
+        }
+        private void btSalvar_Click(object sender, RoutedEventArgs e)
+        {
+            _despesa.Valor = Convert.ToInt32(txtValor.Text);
+            if (dpDataVen.SelectedDate != null)
+            {
+                _despesa.Data_Vencimento = dpDataVen.SelectedDate;
+            }
+            if (dpDataVen.SelectedDate != null)
+            {
+                _despesa.Data_Pagamento = dpDataPag.SelectedDate;
+            }
+            _despesa.Forma_Pagamento = txtFormaPagamento.Text;
+            _despesa.Descricao = txtDescricao.Text;
+
+
+            try
+            {
+                var dao = new DespesaDAO();
+                if (_despesa.Id > 0)
+                {
+                    dao.Update(_despesa);
+                    MessageBox.Show("Informações Atualizadas com Sucesso", "Cadastro Atualizado", MessageBoxButton.OK, MessageBoxImage.Information);
+                    var form = new WindowDespesaList();
+                    form.Show();
+                    
+                }
+                else
+                {
+                    dao.Insert(_despesa);
+                    MessageBox.Show("Informações Salvas com Sucesso", "Cadastro Salvo", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
 
