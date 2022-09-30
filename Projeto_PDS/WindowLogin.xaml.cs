2c0c96daa7f219fdf9a9502310e68cc422b12ea6
Lucas_Furtado_Souza <lucasfurtadosouza32@gmail.com>
2022-09-30 18:23:51 -0400

    Tela Relatorio

--- Projeto_PDS/WindowLogin.xaml.cs
+++ Projeto_PDS/WindowLogin.xaml.cs
@@ -33,7 +33,6 @@ namespace Projeto_PDS
         {
             CarregarListagem();
         }
-
         private void CarregarListagem()
         {
             try
@@ -47,7 +46,6 @@ namespace Projeto_PDS
                 MessageBox.Show(ex.Message);
             }
         }
-
         private void btLogin_Click(object sender, RoutedEventArgs e)
         {
             string email = txtUsuario.Text;
@@ -64,78 +62,6 @@ namespace Projeto_PDS
                 MessageBox.Show("Usuario e/ou senha incorretos! Tente novamente", "Autorização negada", MessageBoxButton.OK, MessageBoxImage.Warning);
                 _ = txtUsuario.Focus();
             }
-            // if (txtSenha.Text == "" && txtUsuario.Text == "")
-            // {
-            //   MessageBox.Show("Digite um usuário ou senha válidos", "Usuário ou Senha inválidos", MessageBoxButton.OK, MessageBoxImage.Error);
-
-            //}
-            // else
-            // {
-            //string salt = getSalt();
-            //string HashPassword = CalcSaltedPass(salt, txtSenha.Text);
-            //string HashUsuario = CalcSaltedPass(salt, txtUsuario.Text);
-            //  string HashUsuario = getHashSha256(txtUsuario.Text);
-            //   string HashPassword = getHashSha256(txtSenha.Text);
-
-            //  MessageBox.Show(HashUsuario);
-            //   MessageBox.Show(HashPassword);
-            //   if (HashUsuario == _login.Nome && HashPassword == _login.Senha)
-            //   {
-            //       var form = new MainWindow();
-            //       form.Show();
-            //   }
-            //      else
-            //     {
-            //         MessageBox.Show("Digite um usuário ou senha válidos", "Usuário ou Senha inválidos", MessageBoxButton.OK, MessageBoxImage.Error);
-            //     }
-            //   }
-
-            //   }
-            // public static string getHashSha256(string text)
-            //  {
-            //   byte[] bytes = Encoding.Unicode.GetBytes(text);
-            //  SHA256Managed hashstring = new SHA256Managed();
-            //  byte[] hash = hashstring.ComputeHash(bytes);
-            //  string hashString = string.Empty;
-            //   foreach (byte x in hash)
-            //   {
-            //        hashString += String.Format("{0:x2}", x);
-            //   }
-            //    return hashString;
-            //   }
-
-            /*public static string getSalt()
-            {
-                var random = new RNGCryptoServiceProvider();
-
-                // Maximum length of salt
-                int max_length = 32;
-
-                // Empty salt array
-                byte[] salt = new byte[max_length];
-
-                // Build the random bytes
-                random.GetNonZeroBytes(salt);
-
-                // Return the string encoded salt
-                return Convert.ToBase64String(salt);
-            }
-
-            public static string SHA256(string randomString)
-            {
-                var crypt = new System.Security.Cryptography.SHA256Managed();
-                var hash = new System.Text.StringBuilder();
-                byte[] crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(randomString));
-                foreach (byte theByte in crypto)
-                {
-                    hash.Append(theByte.ToString("x2"));
-                }
-                return hash.ToString();
-            }
-            public static string CalcSaltedPass(string salt, string password)
-            {
-                return SHA256(salt + password);
-            }*/
         }
     }
 }
