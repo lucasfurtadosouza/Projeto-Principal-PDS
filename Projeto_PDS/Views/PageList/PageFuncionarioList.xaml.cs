ec9151a973e7f6f7c04f390261e7ab6153e3a422
moria10092004 <102737558+moria10092004@users.noreply.github.com>
2022-11-22 15:44:26 -0400

    update de fornecedor e funcionario

--- Projeto_PDS/Views/PageList/PageFuncionarioList.xaml.cs
+++ Projeto_PDS/Views/PageList/PageFuncionarioList.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Projeto_PDS.Models;
+using Projeto_PDS.Views_MessageBox;
 
 namespace Projeto_PDS.Views.PageList
 {
@@ -45,16 +46,18 @@ namespace Projeto_PDS.Views.PageList
         private void btRemover_Click(object sender, RoutedEventArgs e)
         {
             var funcionarioSelecionado = dtFuncionario.SelectedItem as Funcionario;
-            var resultado = MessageBox.Show($"Deseja realmente excluir o funcionário '{funcionarioSelecionado.Nome}'?", "Confirmar Exclusão",
-                MessageBoxButton.YesNo, MessageBoxImage.Warning);
+            var message = new WindowMessageBoxPergunta($"Deseja realmente excluir o funcionario'{funcionarioSelecionado.Id}'?", "Confirmar Exclusão");
+            message.ShowDialog();
+            var resultado = message.retorno;
             try
             {
-                if (resultado == MessageBoxResult.Yes)
+                if (resultado != false)
                 {
                     var dao = new FuncionarioDAO();
                     dao.Delete(funcionarioSelecionado);
 
-                    MessageBox.Show("Registro deletado com sucesso!");
+                    var messageCheck = new WindowMessageBoxCerto("Registro deletado com sucesso!", "Registro Excluído");
+                    messageCheck.ShowDialog();
                     CarregarListagem();
                 }
             }
